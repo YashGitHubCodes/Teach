8a9b86d03e752df4e65cfa89c141a15eef92a9b8
Yash <yash4uson@gmail.com>
2023-01-21 19:36:26 +0530

    Version 1.0

--- src/First/Main.java
+++ src/First/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
-		System.out.println("Version 2.0");
+		System.out.println("Version 1.0");
 	}
 
 }
